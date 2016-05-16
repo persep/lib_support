fba4509d945f8d189ce1cd73e9c3343528b19dce
Максим Третьяков <max@tretyakov-ma.ru>
2016-05-16 21:34:47 +0500

    fix

--- lib/lib_support.rb
+++ lib/lib_support.rb
@@ -15,12 +15,14 @@ require 'refs_controller'
 require 'base_object'
 require 'generators/refs_controller/refs_controller_generator'
 require 'engine'
+require 'active_record/relation/query_methods'
 
 module ActiveRecord
   module QueryMethods
     # full text search
     def find_objects(txt, *opts)
       text = txt.strip.gsub('$', '')
+      return self if text.empty?
 
       res = where("#{table_name}.txt_index @@ plainto_tsquery(unaccent($$#{text}$$))").order("ts_rank(#{table_name}.txt_index, plainto_tsquery($$#{text}$$)) desc")
       res = res.order("#{table_name}.name <-> $$#{text}$$") if column_names.include?('name')
