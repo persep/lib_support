7d607f20edd36a524d9dd968736a65b4ff1578f3
Максим Третьяков <max@tretyakov-ma.ru>
2016-05-27 23:04:14 +0500

    fix

--- lib/refs_controller.rb
+++ lib/refs_controller.rb
@@ -83,8 +83,6 @@ module LibSupport::RefsController
   end
 
   def show
-    @item = find_item_from_params
-
     respond_to do |format|
       format.js do
         render body: nil, status: :not_found and return unless @item && @item.permit?(permission_params)
@@ -96,8 +94,6 @@ module LibSupport::RefsController
   end
 
   def update
-    @item = find_item_from_params
-
     return unless check_modify_permissions
     after_change 'show', update_resource(resource_params)
   end
@@ -157,10 +153,11 @@ module LibSupport::RefsController
 
     base.resource = ActiveRecord::Base
     base.ref_options = {}
+    base.before_action :find_item_from_params, :only => [:show, :update]
   end
 
   def find_item_from_params
-    resource.find_by(id_column => params[id_column])
+    @item = resource.find_by(id_column => params[id_column])
   end
 
   def permit_modify_object?(obj)
--- lib/version.rb
+++ lib/version.rb
@@ -1,3 +1,3 @@
 module LibSupport
-  VERSION = '0.2.79'
+  VERSION = '0.2.80'
 end
